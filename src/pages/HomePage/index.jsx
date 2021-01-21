639ecc30008dca2ba58b7ad2eb682a62209d93c7
Steff <stefaniemarie@gmail.com>
2021-01-21 01:15:24 +0100

    added folders and files for the other pages

--- src/pages/HomePage/index.jsx
+++ src/pages/HomePage/index.jsx
@@ -1,7 +1,7 @@
 import React, { useState, useEffect } from 'react';
 import Cosmic from 'cosmicjs';
+import HomeMainBase from './HomeMainBase';
 import Ellipse from '../../other/Ellipse';
-import styled from 'styled-components';
 import Portrait from '../../other/Portrait.png';
 
 const HomePage = () => {
@@ -37,58 +37,12 @@ const HomePage = () => {
 	}
 
 	return (
-		<MainBase>
+		<HomeMainBase>
 			{pageData === null ? renderSkeleton() : renderPage()}
 			<Ellipse id="ellipse" />
 			<img src={Portrait} />
-		</MainBase>
+		</HomeMainBase>
 	);
 };
 
-const MainBase = styled.main`
-	position: relative;
-	overflow: hidden;
-
-	img {
-		width: 525px;
-		position: absolute;
-		left: 5%;
-		top: 5%;
-	}
-
-	#ellipse {
-		border: 1px solid black;
-	}
-
-	article {
-		width: 50%;
-		z-index: 10;
-		position: absolute;
-		top: 35%;
-		left: 40%;
-
-		h1 {
-			font-size: 3.5rem;
-			margin: 0;
-			padding: 0;
-			font-weight: normal;
-		}
-
-		h1:nth-child(2) {
-			font-size: 8rem;
-			font-weight: normal;
-		}
-
-		p {
-			font-size: 2.5rem;
-			padding: 0;
-			margin: 0;
-		}
-	}
-`;
-
 export default HomePage;
-
-//<Ellipse id="ellipse" />
-
-//{pageData === null ? renderSkeleton() : renderPage()}
